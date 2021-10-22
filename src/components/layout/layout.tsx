6ffa4f7d0345ce13d93258be2906b17e7841448d
Armados <predomelo@gmail.com>
2021-10-22 18:01:34 +0300

    added game state toggle to the layout component, and simple menu styling

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -4,18 +4,47 @@ import Board from '../board/board';
 import { CellInterface } from '../../models/cellInterface';
 import { ShapePropsInterface } from '../../models/shapeInterface';
 import { boardProperties } from '../../services/boardService';
+import { shapeProperties } from '../../services/shapeService';
 
 const Layout = () => {
 
     const [board, setBoard] = useState<CellInterface[][]>([]);
     const [shape, setShape] = useState<ShapePropsInterface>();
+    const [gameState, setGameState] = useState<string>("");
+
+    const toggleGameState = (): void => {
+
+        if (gameState == "" || gameState == "end") {
+
+            let initBoard = boardProperties.boardInitialization();
+            boardProperties.board = initBoard;
+            setBoard(initBoard);
+            setGameState("start");
+            let newShapeType = shapeProperties.pickShape();
+            let newShape = shapeProperties.shapeInitialization(newShapeType);
+            shapeProperties.location = newShape.location;
+            shapeProperties.shape = newShape.shape;
+            setShape(newShape);
+
+        }
+
+        else {
+
+            setGameState("end");
+            setShape(undefined);
+
+        }
+
+    }
 
     return (
-        <div className = "layout">
+        <div className = "layout" role = "button">
             <div className = "tetrisContainer">
                 <Board board = {board} />
             </div>
-            <div className = "tetrisMenu"></div>
+            <div className = "tetrisMenu">
+            {gameState == "" || gameState == "end" ? <button className = "stateButton" onClick={toggleGameState}>Start</button> : <button className = "stateButton" onClick={toggleGameState}>Concede</button>}
+            </div>
         </div>
     )
 
